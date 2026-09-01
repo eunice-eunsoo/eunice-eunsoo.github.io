3ec87d77ee1c513d7f447e70775fecea977e1bdd
Eunsoo Lee <eunicelee043@gmail.com>
2026-09-01 00:41:18 -0500

    changed position of person image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,12 +2,25 @@ function Footer() {
   return (
     <footer id="contact">
       <div className="content-container">
-        <div className="footer-main"><p>You made it to the bottom! thank youuu</p><a href="mailto:eunicelee043@gmail.com">eunicelee043@gmail.com ↗</a></div>
-        <div className="footer-content"><p>p.s. my website is food themed because I love cooking and eating !</p></div>
-        <div className="footer-bottom"><span>© {new Date().getFullYear()} Eunice Lee</span><a href="#top">Back to top ↑</a></div>
+        <div className="footer-main">
+          <p>You made it to the bottom! thank youuu</p>
+          <a style={{ fontSize: "1rem" }} href="https://drive.google.com/file/d/1PBCGJkdMvfn0kMQJ_g3868qMX6mgxMij/view?usp=sharing">
+            {" "}
+            My Resume ↗
+          </a>
+        </div>
+        <div className="footer-content">
+          <p>
+            p.s. my website is food themed because I love cooking and eating !
+          </p>
+        </div>
+        <div className="footer-bottom">
+          <span>© {new Date().getFullYear()} Eunice Lee</span>
+          <a href="#top">Back to top ↑</a>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
